b6105e71e3e5316cb369211f599ec6c3a820c32c
Rishabh <140707297+rishabhrawat05@users.noreply.github.com>
2024-07-22 15:02:36 +0530

    Delete src directory

--- src/main/java/com/ecommerce/dropify/repository/CustomerSupportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.dropify.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.dropify.model.CustomerSupport;
-
-public interface CustomerSupportRepository extends JpaRepository<CustomerSupport, Long> {
-
-}
